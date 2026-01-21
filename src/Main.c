c6cc799cc97b4c744b5bd78c86bd2227f99620f6
codeleaded <gnu.c.reaper@gmail.com>
2026-01-22 00:22:53 +0100

    init commit

---
+++ src/Main.c
@@ -0,0 +1,22 @@
+#include "/home/codeleaded/System/Static/Library/WindowEngine1.0.h"
+
+void Setup(AlxWindow* w){
+    
+}
+void Update(AlxWindow* w){
+    
+    
+    Clear(BLACK);
+    
+
+}
+void Delete(AlxWindow* w){
+
+}
+
+int main() {
+    if(Create("3D Wrapper",1000,1000,1,1,Setup,Update,Delete)){
+        Start();
+    }
+    return 0;
+}
